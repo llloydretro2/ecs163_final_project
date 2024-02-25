fb4792cb23cc898c25c64842d6224208fceb7bbd
BoquanFang <david.fang200251@gmail.com>
2024-02-24 16:37:04 -0800

    Keep the top ten manufacturers. Can start to render stream graph.

--- data/population_count.py
+++ data/population_count.py
@@ -13,10 +13,31 @@ def reset_EV_population():
     model_counts.reset_index(inplace=True)
 
     # Save the resulting dataset to a CSV file
-    model_counts.to_csv("./processed_data/general/Electric_Vehicle_Population_Count.csv", index=False)
+    model_counts.to_csv("./unprocessed_data/Electric_Vehicle_Population_Count.csv", index=False)
+
+def keep_top_ten():
+    df = pd.read_csv("./unprocessed_data/Electric_Vehicle_Population_Count.csv")
+    # Filter rows for the year 2024
+    df_2024 = df[df['Model_Year'] == 2024]
+
+    # Calculate the total counts for each category in 2024
+    total_counts_2024 = df_2024.sum(axis=0)
+
+    # Sort the categories by counts and select the top ten largest categories
+    top_categories = total_counts_2024.nlargest(10)
+
+    # Get the column names of the top ten categories
+    top_categories_columns = top_categories.index
+
+
+    # Add 'Model_Year' back to the DataFrame
+    df_2024_filtered = df[list(top_categories_columns)]
+
+    df_2024_filtered.to_csv("./processed_data/general/Electric_Vehicle_Population_Count.csv", index=False)
 
 def main():
     reset_EV_population()
+    keep_top_ten()
 
 if __name__ == '__main__':
     main()
