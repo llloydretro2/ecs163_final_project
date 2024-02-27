fc4fc8fb677796bcc7ef35e3a97882fc4c24b642
megasceptileftw <84885375+megasceptileftw@users.noreply.github.com>
2024-02-27 09:20:55 -0800

    Added parallel plot
    Lines not showing up for some reason, needs to be fixed

---
+++ code/parallel_plot.js
@@ -0,0 +1,92 @@
+// Dimensions for Parallel Plot
+let parallelLeft = (1/5) * width, parallelTop = (1/5) * height;
+let parallelMargin = {top: 10, right: 30, bottom: 30, left: 60},
+    parallelWidth = (1/2) * width - parallelMargin.left - parallelMargin.right,
+    parallelHeight = (1/2) * height - parallelMargin.top - parallelMargin.bottom;
+
+let svg3 = d3.select("#svg3");
+
+d3.csv("../data/processed_data/general/Electric_Vehicle_Specification_Data.csv").then(rawData =>{
+    console.log("rawData", rawData);
+
+    const g1 = svg3.append("g")
+                .attr("width", parallelWidth + parallelMargin.left + parallelMargin.right)
+                .attr("height", parallelHeight + parallelMargin.top + parallelMargin.bottom)
+                .attr("transform", `translate(${parallelMargin.left}, ${height - (parallelHeight + parallelMargin.top + parallelMargin.bottom)})`);
+
+    // For the parallel plot, we only care about these attributes
+    let dimensions = ["PriceDollars", "AccelSec","TopSpeed_KmH", "Range_Km", "Efficiency_WhKm", "FastCharge_KmH", "Seats"];
+
+    // store y objects
+    const y = {};
+    for (let i in dimensions) {
+        let name = dimensions[i];
+        y[name] = d3.scaleLinear()
+        .domain( d3.extent(rawData, function(d) { return +d[name]; }))
+        .range([parallelHeight, 0]);
+    }
+
+    // Build the X scale -> it find the best position for each Y axis
+    const x = d3.scalePoint()
+        .range([0, parallelWidth])
+        .padding(1)
+        .domain(dimensions);
+
+    function path(d) {
+        return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+    }
+
+    // Draw the lines
+    g1.selectAll("myPath")
+    .data(rawData)
+    .enter().append("path")
+    .attr("d",  path)
+    .style("fill", "none")
+    .style("stroke", "")
+    .style("opacity", 0.5)
+
+    // Draw the axis:
+    g1.selectAll("myAxis")
+    // For each dimension of the dataset I add a 'g' element:
+    .data(dimensions).enter()
+    .append("g")
+    // I translate this element to its right position on the x axis
+    .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+    // And I build the axis with the call function
+    .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+    // Add axis title
+    .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", -9)
+        .text(function(d) { return d; })
+        .style("fill", "black")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}).catch(function(error){
+    console.log(error);
+});
