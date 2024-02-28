0f9e6733fabed695a11798494a7d07e58e34678b
Amon <lloydretro2@gmail.com>
2024-02-27 18:32:12 -0800

    update map.js with title, selection highlight, spectrum, and state names

--- code/map.js
+++ code/map.js
@@ -5,35 +5,7 @@ let mapMargin = {top: 10, right: 30, bottom: 30, left: 60},
     mapWidth = width - mapMargin.left - mapMargin.right,
     mapHeight = height - mapMargin.top - mapMargin.bottom;
 
-// Load the TopoJSON data asynchronously
-// d3.json('https://d3js.org/us-10m.v2.json').then(function(us) {
-//     // Now 'us' is available and can be used
-//     let data = topojson.feature(us, us.objects.states).features;
-
-//     // Continue with your map drawing logic here, using 'data'
-//     // For example, appending paths for each state feature to the SVG
-//     const svg = d3.select("#svg2")
-
-//     const projection = d3.geoAlbersUsa()
-//         .translate([mapWidth / 2, mapHeight / 2])
-//         .scale([mapWidth]); // Adjust scale according to your needs
-
-//     const path = d3.geoPath()
-//         .projection(projection);
-
-//     svg.selectAll("path")
-//         .data(data)
-//         .enter().append("path")
-//         .attr("d", path)
-//         .attr("fill", "#ccc")
-//         .attr("stroke", "#333")
-//         .attr("stroke-width", "0.5");
-// }).catch(function(error) {
-//     console.log(error);
-// });
-
 // Setting up the svg element for D3 to draw in
-
 map = d3.select("#svg2")
     .attr("width", width)
     .attr("height", height)
@@ -41,7 +13,7 @@ map = d3.select("#svg2")
 // A projection tells D3 how to orient the GeoJSON features
 let usaProjection = d3.geoAlbersUsa()
   .scale(1000)
-  .translate([width/2, height/2])
+  .translate([width/3, height/2])
 let europeProjection = d3.geoMercator()
   .center([ 13, 52 ])
   .scale([ width / 1.5 ])
@@ -64,6 +36,15 @@ if (usa) {
   geoJsonUrl = "https://gist.githubusercontent.com/spiker830/3eab0cb407031bf9f2286f98b9d0558a/raw/7edae936285e77be675366550e20f9166bed0ed5/europe_features.json"
 }
 
+map.append("text")
+    .attr("x", width / 3) // Center horizontally
+    .attr("y", height/7) // Position from the top
+    .attr("text-anchor", "middle") // Center the text at (x, y)
+    .style("font-size", "24px") // Example font size, adjust as needed
+    .attr("class", "map-title")
+    .attr("font-family", "serif")
+    .text("Interactive Map of Charging Station Counts in the USA"); // Your title
+
 
 d3.csv('../data/processed_data/general/state_counts_sorted.csv').then(data => {
 
@@ -75,11 +56,55 @@ d3.csv('../data/processed_data/general/state_counts_sorted.csv').then(data => {
       statesArray.push(d.state)
   });
 
-  // console.log(stationCountByState)
-  // console.log(statesArray)
-
   let colorScale = d3.scaleSequential(d3.interpolateBlues)
       .domain(d3.extent(data, d => +d.number)); // Use the extent of station numbers for domain
+  
+  const legendWidth = 300, legendHeight = 20, legendMargin = {top: 20, right: 20, bottom: 20, left: 20};
+
+  // Create a legend container
+  let legend = map.append("g")
+    .attr("class", "color-legend")
+    .attr("transform", `translate(${width /5}, ${height/8*7})`);
+
+  // Create a gradient for the legend
+  const gradient = legend.append("defs")
+    .append("linearGradient")
+    .attr("id", "gradient")
+    .attr("x1", "0%")
+    .attr("x2", "100%")
+    .attr("y1", "0%")
+    .attr("y2", "0%");
+
+  colorScale.ticks().forEach((t, i, n) => {
+    gradient.append("stop")
+      .attr("offset", `${(i/n.length) * 100}%`)
+      .attr("stop-color", colorScale(t));
+  });
+
+  // Add the gradient bar
+  legend.append("rect")
+    .attr("width", legendWidth)
+    .attr("height", legendHeight)
+    .style("fill", "url(#gradient)");
+
+  // Add legend labels
+  legend.append("text")
+    .attr("class", "legend-text")
+    .attr("x", 0)
+    .attr("class", "map-title")
+    .attr("y", legendHeight + legendMargin.bottom)
+    .text(d3.min(colorScale.domain()));
+
+  legend.append("text")
+    .attr("class", "legend-text")
+    .attr("x", legendWidth)
+    .attr("class", "map-title")
+    .attr("y", legendHeight + legendMargin.bottom)
+    .attr("text-anchor", "end")
+    .text(d3.max(colorScale.domain()));
+
+    const smallStates = ["RI", "DE"];
+    const textOffset = 50;
 
   d3.json(geoJsonUrl).then(geojson => {
     map.selectAll("path")
@@ -90,38 +115,87 @@ d3.csv('../data/processed_data/general/state_counts_sorted.csv').then(data => {
           .attr("stroke", "grey")
           .attr("fill", d => {
               let count = stationCountByState[d.id];
-              return count ? colorScale(count) : "white"; // Default to white if no data
+              return count ? colorScale(count) : "white";
           })
           .on("click", function(event, d) {
+        
+            map.select("text:not(.map-title)").remove();
+            map.selectAll("path")
+              .attr("stroke-width", 1)
+              .attr("stroke", "gray")
             
-            // let stateName = d.properties.id;
-            // console.log(statesArray[d])
-            // console.log(stationCountByState[statesArray[d]])
-
-
-            map.select("text").remove();
+            d3.select(this)
+            .attr("stroke-width", 3) // Increase stroke width to highlight
+            .attr("stroke", "red");
 
               // Append new text element at the mouse position
             mapText = map.append("text")
-                .attr("x", width/10)
-                .attr("y", height/8)
                 .attr("fill", "black")
                 .style("font-size", "16px")
                 .attr("font-family", "serif")
                 .style("pointer-events", "none")
                 
             mapText.append("tspan")
-              .attr("x", width/10)
-              .attr("y", height/8)
+              .attr("x", width/2)
+              .attr("y", height/8*7)
                 .text('state: ' + statesArray[d])
             
             mapText.append("tspan")
-                .attr("x", width/10)
-                .attr("y", height/7)
+                .attr("x", width/2)
+                .attr("y", height/8*7 + 20)
                 .text('number: ' + stationCountByState[statesArray[d]])
-            
-            
-            
+        });
+        
+        map.selectAll(".state-name")
+          .data(geojson.features.filter(d => !smallStates.includes(d.id)))
+          .enter()
+          .append("text")
+          .attr("class", "map-title")
+          .attr("font-family", "serif")
+          .attr("transform", function(d) {
+            let centroid = pathGenerator.centroid(d);
+            if (d.id === "LA") {
+                centroid[0] -= 10;
+            }
+            if (d.id === "HI") {
+              centroid[0] += 10;
+              centroid[1] += 10;
+            }
+            if (d.id === "FL") {
+              centroid[0] += 10;
+            }
+            return `translate(${centroid})`;
+        })
+          .attr("text-anchor", "middle")
+          .attr("alignment-baseline", "middle")
+          .style("font-size", "10px")
+          .text(d => d.id);
+
+        geojson.features.forEach(feature => {
+          if (smallStates.includes(feature.id)) { // Adjust the condition based on your needs
+            const centroid = pathGenerator.centroid(feature);
+      
+            // Calculate text position; this is a simplistic approach
+            const textPosition = [centroid[0] + textOffset, centroid[1]];
+      
+            // Draw line from centroid to text position
+            map.append("line")
+              .attr("x1", centroid[0])
+              .attr("y1", centroid[1])
+              .attr("x2", textPosition[0])
+              .attr("y2", textPosition[1])
+              .attr("stroke", "black")
+              .style("stroke-dasharray", ("3, 3"));
+      
+            // Add text at textPosition
+            map.append("text")
+              .attr("x", textPosition[0]+ 2)
+              .attr("y", textPosition[1]+2)
+              .attr("class", "map-title")
+              .attr("text-anchor", "start")
+              .style("font-size", "8px")
+              .text(feature.id);
+          }
         });
   });
 });
